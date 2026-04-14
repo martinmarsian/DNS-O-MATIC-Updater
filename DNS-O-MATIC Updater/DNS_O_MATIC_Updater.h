3a629d672bc9e97ec07714c2cc5fcfc9a384e71d
Professor© <martin.marsia@aol.com>
2026-04-14 13:48:09 +0200

    Initial Commit

---
+++ DNS-O-MATIC Updater/DNS_O_MATIC_Updater.h
@@ -0,0 +1,14 @@
+//
+//  DNS_O_MATIC_Updater.h
+//  DNS-O-MATIC Updater
+//
+//  Created by Jörg Märtin on 14.04.26.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface DNS_O_MATIC_Updater : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ DNS-O-MATIC Updater/DNS_O_MATIC_Updater.m
@@ -0,0 +1,16 @@
+//
+//  DNS_O_MATIC_Updater.m
+//  DNS-O-MATIC Updater
+//
+//  Created by Jörg Märtin on 14.04.26.
+//
+
+#import "DNS_O_MATIC_Updater.h"
+
+@implementation DNS_O_MATIC_Updater
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
